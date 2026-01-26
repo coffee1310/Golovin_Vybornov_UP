f0be6ddd94504f7b61642841d843182ba1ee4899
coffee13102006 <coffee13102006@yandex.ru>
2026-01-26 23:31:33 +0300

    first commit

---
+++ PropertyManagement/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using PropertyManagement.Pages;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PropertyManagement
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Загружаем главную страницу при старте
+            MainFrame.Navigate(new MainPage());
+        }
+
+        private void MainPage_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new MainPage());
+        }
+
+        private void BuildingsPage_Click(object sender, RoutedEventArgs e)
+        {
+            // Временная заглушка
+            MessageBox.Show("Страница 'Здания' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ApartmentsPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Квартиры' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void OwnersPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Собственники' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void EmployeesPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Сотрудники' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ServiceRequestsPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Заявки' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void NewRequestPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Новая заявка' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void SettingsPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Настройки' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void HelpPage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Справка' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ PropertyManagement/Pages/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PropertyManagement.Pages
+{
+    public partial class MainPage : Page
+    {
+        private readonly PropertyManagementEntities _context;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _context = new PropertyManagementEntities();
+            LoadStatistics();
+            LoadRecentRequests();
+        }
+
+        private void LoadStatistics()
+        {
+            try
+            {
+                // Загрузка статистики из базы данных
+                var buildingsCount = _context.Buildings.Count();
+                var apartmentsCount = _context.Apartments.Count();
+                var employeesCount = _context.Employees.Count();
+                var requestsCount = _context.ServiceRequests.Count();
+
+                BuildingsCount.Text = buildingsCount.ToString();
+                ApartmentsCount.Text = apartmentsCount.ToString();
+                EmployeesCount.Text = employeesCount.ToString();
+                RequestsCount.Text = requestsCount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки статистики: {ex.Message}", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
+                // Устанавливаем значения по умолчанию
+                BuildingsCount.Text = "0";
+                ApartmentsCount.Text = "0";
+                EmployeesCount.Text = "0";
+                RequestsCount.Text = "0";
+            }
+        }
+
+        private void LoadRecentRequests()
+        {
+            try
+            {
+                // Загрузка последних заявок из базы данных (без Include)
+                var recentRequests = _context.ServiceRequests
+                    .OrderByDescending(r => r.request_id)
+                    .Take(5)
+                    .ToList();
+
+                RecentRequestsGrid.ItemsSource = recentRequests;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки заявок: {ex.Message}", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void AddBuilding_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функция 'Добавить здание' будет доступна на странице 'Здания'", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void AddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функция 'Добавить сотрудника' будет доступна на странице 'Сотрудники'", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void CreateRequest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функция 'Создать заявку' будет доступна на странице 'Новая заявка'", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ViewReports_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функция 'Просмотреть отчеты' в разработке", "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ViewAllRequests_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем главное окно
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow != null)
+            {
+                // Переходим на страницу заявок
+                //mainWindow.NavigateToServiceRequests();
+            }
+        }
+    }
+}
